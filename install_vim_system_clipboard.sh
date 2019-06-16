143dbe5df8369f0b766049af3a6cea4269fc5dc1
Filip Osowski <filiposowski5@gmail.com>
2019-06-16 00:14:07 +0000

    Add script to setup system clipboard for Vim

---
+++ install_vim_system_clipboard.sh
@@ -0,0 +1,17 @@
+!#/bin/sh
+echo "export DISPLAY=:0.0" >> ~/.bashrc
+
+echo "
+[Unit]
+Description=Turn on Xvfb for system (X-server) clipboard in Vim
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/usr/bin/Xvfb :0.0
+Restart=always
+" >> xvfb.service
+
+sudo systemctl enable xvfb.service
+sudo systemctl start xvfb.service
